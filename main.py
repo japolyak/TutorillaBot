0fcbd4bd409f3a38cd9810c27e234b56a798e8b6
japolyak <artemkryvolap@gmail.com>
2023-12-03 20:08:30 +0100

    feat(start) Bot start updated

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from bot.bot_token import bot
 import logging
 from bot.handlers import registration, localization
+from bot.handlers.tutor import reply_tutor_keyboard
+from bot.handlers.student import reply_student_keyboard
+from bot.handlers import shared
+
 
 logging.basicConfig(encoding='utf-8', level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
 
