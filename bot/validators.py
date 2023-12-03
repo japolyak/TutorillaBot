0fe02958737c91f20a3b8859bde1e8df978256a0
japolyak <artemkryvolap@gmail.com>
2023-12-03 20:28:05 +0100

    feat(markup) Some other things

---
+++ bot/validators.py
@@ -0,0 +1,13 @@
+import re
+
+
+class Validator:
+    @staticmethod
+    def validate_name(name):
+        return re.match("^[a-zA-Zа-яА-Я ]+$", name)
+
+    @staticmethod
+    def email_validator(email):
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+
+        return re.fullmatch(regex, email)
