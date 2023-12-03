618249901b115c08e145eaee85046a1faccd60cf
japolyak <artemkryvolap@gmail.com>
2023-12-03 20:18:48 +0100

    feat(handlers) Handlers implemented and separated

---
+++ bot/handlers/student/reply_student_keyboard.py
@@ -0,0 +1,74 @@
+from telebot import types
+from bot.api.clients.student_client import StudentClient
+from bot.bot_token import bot
+from bot.markups.inline_keyboard_markups import InlineKeyboardMarkupCreator
+from bot.markups.reply_keyboard_markup import ReplyKeyboardMarkupCreator
+from ...serializers.serializers import SubjectSerializer
+
+
+@bot.message_handler(regexp="Classroom")
+def restore_redis(message: types.Message):
+    markup = ReplyKeyboardMarkupCreator.student_classroom_markup()
+
+    bot.send_message(chat_id=message.chat.id, text="Your classroom is here", reply_markup=markup)
+
+
+@bot.message_handler(regexp="My classes")
+def my_courses(message: types.Message):
+    request = StudentClient.my_classes(user_id=message.from_user.id)
+
+    msg_text = "Choose your subject"
+
+    response_data = SubjectSerializer.serialize(request.json())
+
+    if not len(response_data):
+        bot.send_message(chat_id=message.chat.id, text="You have no courses")
+        return
+
+    markup = InlineKeyboardMarkupCreator.student_courses_markup(courses=response_data)
+
+    bot.send_message(chat_id=message.chat.id, text=msg_text, reply_markup=markup)
+
+
+@bot.message_handler(regexp="Subscribe course")
+def subscribe_course(message: types.Message):
+    send_available_subjects(user_id=message.from_user.id)
+
+
+@bot.callback_query_handler(func=lambda call: (call.data.startswith("Subscribe course")))
+def subscribe_course_callback(call: types.CallbackQuery):
+    course_id = int(call.data.split(" ")[2])
+
+    request = StudentClient.enroll_in_course(user_id=call.from_user.id, course_id=course_id)
+
+    if not request.ok:
+        bot.send_message(chat_id=call.from_user.id, text="Problem occurred")
+        return
+
+    markup = ReplyKeyboardMarkupCreator.student_classroom_markup()
+    bot.send_message(chat_id=call.from_user.id, text="You have successfully subscribed to the course", reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: (call.data.startswith("Return to select")))
+def return_to_select_callback(call: types.CallbackQuery):
+    send_available_subjects(user_id=call.from_user.id)
+
+
+def send_available_subjects(user_id: int):
+    request = StudentClient.available_courses_student(user_id=user_id)
+
+    if not len(request.json()):
+        bot.send_message(chat_id=user_id, text="No available subjects")
+        return
+
+    response_data = SubjectSerializer.serialize(request.json())
+
+    if not len(response_data):
+        bot.send_message(chat_id=user_id, text="No available subjects")
+        return
+
+    msg_text = "Choose subject to learn"
+
+    markup = InlineKeyboardMarkupCreator.sub_course_markup(courses=response_data)
+
+    bot.send_message(chat_id=user_id, text=msg_text, reply_markup=markup)
---
+++ bot/handlers/tutor/reply_tutor_keyboard.py
@@ -0,0 +1,69 @@
+from telebot import types
+from bot.api.clients.tutor_client import TutorClient
+from bot.bot_token import bot
+from bot.markups.inline_keyboard_markups import InlineKeyboardMarkupCreator
+from bot.markups.reply_keyboard_markup import ReplyKeyboardMarkupCreator
+from ...serializers.serializers import SubjectSerializer
+
+
+@bot.message_handler(regexp="Office")
+def restore_redis(message: types.Message):
+    markup = ReplyKeyboardMarkupCreator.tutor_office_markup()
+    bot.send_message(chat_id=message.chat.id, text="Office is here", reply_markup=markup)
+
+
+@bot.message_handler(regexp="My courses")
+def my_courses(message: types.Message):
+
+    request = TutorClient.my_courses(user_id=message.from_user.id)
+
+    msg_text = "Choose your course"
+
+    response_data = SubjectSerializer.serialize(request.json())
+
+    if not len(response_data):
+        bot.send_message(chat_id=message.chat.id, text="You have no courses")
+        return
+
+    markup = InlineKeyboardMarkupCreator.tutor_courses_markup(courses=response_data)
+
+    bot.send_message(chat_id=message.chat.id, text=msg_text, reply_markup=markup)
+
+
+@bot.message_handler(regexp="Add course")
+def add_course(message: types.Message):
+    request = TutorClient.available_subjects_tutor(user_id=message.from_user.id)
+
+    if not len(request.json()):
+        bot.send_message(chat_id=message.chat.id, text="No available subjects")
+        return
+
+    response_data = SubjectSerializer.serialize(request.json())
+
+    if not len(response_data):
+        bot.send_message(chat_id=message.chat.id, text="No available subjects")
+        return
+
+    msg_text = "Choose course to teach"
+
+    markup = InlineKeyboardMarkupCreator.add_course_markup(courses=response_data)
+
+    bot.send_message(chat_id=message.chat.id, text=msg_text, reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: (call.data.startswith("Add course")))
+def add_course_callback(call: types.CallbackQuery):
+    subject_id = int(call.data.split(" ")[2])
+
+    request = TutorClient.add_course(user_id=call.from_user.id, subject_id=subject_id)
+
+    if not request.ok:
+        request = TutorClient.available_subjects_tutor(user_id=call.from_user.id)
+
+        response_data = SubjectSerializer.serialize(request.json())
+
+        markup = InlineKeyboardMarkupCreator.add_course_markup(courses=response_data)
+        bot.send_message(chat_id=call.from_user.id, text="Oops, try again or try later", reply_markup=markup)
+        return
+
+    bot.send_message(chat_id=call.from_user.id, text="Course added successfully")
