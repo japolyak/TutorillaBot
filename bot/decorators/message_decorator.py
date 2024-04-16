1e138ed07cf95e536607008f5d599a1220466925
japolyak <artemkryvolap@gmail.com>
2024-04-16 22:14:16 +0200

    test

--- bot/decorators/message_decorator.py
+++ bot/decorators/message_decorator.py
@@ -1,4 +1,5 @@
 from bot.redis.redis_client import r
+from telebot.types import Message
 
 
 class MessageDecorator:
@@ -23,6 +24,49 @@ class MessageDecorator:
 
     @staticmethod
     def admin_guard(chat_id):
-        is_admin = r.hget(chat_id, "is_admin")
+        is_student = r.hget(chat_id, "is_admin")
 
-        return is_admin == "1"
+        return is_student == "1"
+
+    @classmethod
+    def is_user_registered(cls, func):
+        def wrapper(message: Message):
+            is_active_state = r.hget(message.from_user.id, "is_active")
+            if is_active_state == "1" or is_active_state == "0":
+                return func(message)
+            return None
+
+        return wrapper
+
+    @classmethod
+    def is_tutor(cls, func):
+        def wrapper(message: Message):
+            is_tutor = r.hget(message.from_user.id, "is_tutor")
+            if is_tutor == "1":
+                return func(message)
+
+            return None
+
+        return wrapper
+
+    @classmethod
+    def is_student(cls, func):
+        def wrapper(message: Message):
+            is_tutor = r.hget(message.from_user.id, "is_student")
+            if is_tutor == "1":
+                return func(message)
+
+            return None
+
+        return wrapper
+
+    @classmethod
+    def is_admin(cls, func):
+        def wrapper(message: Message):
+            is_admin = r.hget(message.from_user.id, "is_admin")
+            if is_admin == "1":
+                return func(message)
+
+            return None
+
+        return wrapper
