7e2897455494664857628e9eab4b72d5571dc02b
japolyak <artemkryvolap@gmail.com>
2024-07-31 00:52:17 +0200

    feat: Migrations fix

--- src/bot/database/migrations/env.py
+++ src/bot/database/migrations/env.py
@@ -5,7 +5,7 @@ from alembic import context
 
 from sqlalchemy import engine_from_config, pool
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../..')))
 
 from src.bot.config import sqlalchemy_database_url
 from src.bot.database.models import Base
