be695ea92a14b09426b0647ebe183a5dddfdcce3
japolyak <artemkryvolap@gmail.com>
2024-12-30 00:32:23 +0100

    feat: Telegram CloudStorage replaced with LocalStorage

--- src/web_app/src/modules/core/composables/app-behaviour.ts
+++ src/web_app/src/modules/core/composables/app-behaviour.ts
@@ -6,30 +6,31 @@ interface AppBehaviour {
 }
 
 export function useAppBehaviour(): AppBehaviour {
-	function handleVersion() {
-		window.Telegram.WebApp.CloudStorage.getItem('appVersion', (error, result) => {
-			const currentVersion: undefined | string = import.meta.env.VITE_APP_VERSION?.replace('v', '');
-			if (StringUtils.isEmpty(currentVersion)) return;
+	const appVersionKey = 'appVersion';
 
-			if (StringUtils.isEmpty(result)) {
-				window.Telegram.WebApp.CloudStorage.setItem('appVersion', currentVersion.replaceAll('.', '-'));
+	function handleVersion() {
+		const currentVersion: undefined | string = import.meta.env.VITE_APP_VERSION?.replace('v', '');
+		if (StringUtils.isEmpty(currentVersion)) return;
+
+		const savedVersion = localStorage.getItem(appVersionKey);
+		if (StringUtils.isEmpty(savedVersion)) {
+			localStorage.setItem(appVersionKey, currentVersion);
+			window.location.reload();
+			return;
+		}
+
+		const currentVersionParts = currentVersion.split('.');
+		const savedVersionParts = savedVersion.split('-');
+
+		if (currentVersionParts.length !== savedVersionParts.length) return;
+
+		for (let i = 0; i < currentVersionParts.length; i++) {
+			if (parseInt(currentVersionParts[i]) > parseInt(savedVersionParts[i])) {
+				localStorage.setItem(appVersionKey, currentVersion);
 				window.location.reload();
 				return;
 			}
-
-			const currentVersionParts = currentVersion.split('.');
-			const savedVersionParts = result.split('-');
-
-			if (currentVersionParts.length !== savedVersionParts.length) return;
-
-			for (let i = 0; i < currentVersionParts.length; i++) {
-				if (parseInt(currentVersionParts[i]) > parseInt(savedVersionParts[i])) {
-					window.Telegram.WebApp.CloudStorage.setItem('appVersion', currentVersion.replaceAll('.', '-'));
-					window.location.reload();
-					return;
-				}
-			}
-		});
+		}
 	}
 
 	return {
